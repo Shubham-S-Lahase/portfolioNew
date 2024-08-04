9e55c1e4e85b444a7061a549aa5e3b308fc5e534
Shubham Lahase <shubhamlahase@gmail.com>
2024-08-04 15:23:50 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import ContactForm from './components/Contact';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
